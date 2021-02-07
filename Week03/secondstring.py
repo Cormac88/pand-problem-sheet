139e8c2d9aedb375f2d44dd4a744a42e7ba4623f
Cormac Hennigan <G00398284@gmit.ie>
2021-02-07 17:26:31 +0000

    uploading problem sheet week03 to the repository

---
+++ Week03/secondstring.py
@@ -0,0 +1,7 @@
+# Program that inputs a string and outputs every second letter in reverse order.
+
+# Author: Cormac Hennigan
+
+sentence = str(input("Please enter a sentence: "))
+
+print(sentence[::-2])
